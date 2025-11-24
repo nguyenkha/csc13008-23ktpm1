2b796ab2bccf683a4c8014fe655fac4aa1800e7f
Kha Do <donguyenkha@gmail.com>
2025-11-24 17:42:29 +0700

    Add shipping form with supabase demo

---
+++ shipping/src/ListOrders.jsx
@@ -0,0 +1,49 @@
+import { useState, useEffect, useContext } from 'react'
+import ApiContext from './ApiContext'
+
+export default function ListOrders({ setCurrentPage }) {
+  // Load API url and key from context
+  const api = useContext(ApiContext);
+  
+  const [orders, setOrders] = useState([]);
+
+  useEffect(() => {
+    // Fetch data with province and ward pre-load
+    fetch(`${api.url}/orders?select=*,ward:wards(*),province:provinces(*)`, {
+      headers: {
+        apikey: api.key,
+      },
+    }).then(async (result) => {
+      if (result.status === 200) {
+        setOrders(await result.json());
+      } else {
+        console.error('Cannot load order data:', result);
+      }
+    });
+  }, []);
+
+  return (
+    <div className="max-w-3xl mx-auto p-4 bg-white shadow-md rounded-md space-y-6">
+      <div className="max-w-3xl mx-auto p-4">
+        <h2 className="text-2xl font-bold mb-4">Order List</h2>
+        <ul className="space-y-4">
+          {orders.map((o) => (
+            <li className="border p-4 rounded shadow hover:shadow-lg transition">
+              <p><span className="font-semibold">Order ID:</span> #{o.id}</p>
+              <p><span className="font-semibold">Recipient:</span> {o.recipient}</p>
+              <p><span className="font-semibold">Address:</span> {o.house_number} {o.street}, {o.ward.name_with_type}, {o.province.name_with_type}</p>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <button
+        type="submit"
+        className="w-full bg-blue-600 text-white py-2 rounded hover:bg-blue-700 transition"
+        // Set page to shipping form
+        onClick={() => setCurrentPage('ShippingForm')}
+      >
+        Create Order
+      </button>
+    </div>
+  )
+}
